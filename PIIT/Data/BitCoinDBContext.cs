765cf98008000f53a1c14c109e98048677484f77
dungntth2009038 <73456416+dungntth2009038@users.noreply.github.com>
2022-04-02 09:30:58 +0700

    update

---
+++ PIIT/Data/BitCoinDBContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PIIT.Data
+{
+    public class BitCoinDBContext: DbContext
+    {
+        public BitCoinDBContext() : base("name = BitcoinDbString")
+        { }   
+        public DbSet<BitCoin> BitCoins { get; set }
+        public DbSet<Market> Markets { get; set }
+    }
+}
---
+++ PIIT/Models/BitCoin.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Coin.Models
+{
+    public class BitCoin
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int MarketId;
+        [Required(ErrorMessage = "Please enter name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter base asset")]
+        public string BaseAsset { get; set; }
+        [Required(ErrorMessage = "Please enter quote asset")]
+        public string QuoteAsset { get; set; }
+        [Required(ErrorMessage = "Please enter last price")]
+        public double LastPrice { get; set; }
+        [Required(ErrorMessage = "Please enter volumn24h")]
+        public double Volumn24h { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public bool Status { get; set; }
+        public virtual Market Market { get; set; }
+    }
+}
